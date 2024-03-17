24a48abd84e4d48e33cb70d2e267cad212f21eee
Sakthiprakashh <147151424+Sakthiprakashh@users.noreply.github.com>
2024-03-17 16:00:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function nav_open() {
+    let nav_bar = document.getElementById("side-nav");
+    nav_bar.style.left = "0%"
+}
+function nav_close() {
+    let nav_bar = document.getElementById("side-nav");
+
+    nav_bar.style.left = "-60%"
+
+
+}
+
+
+function ss() {
+    let popupp = document.getElementById("pop-up");
+    popupp.style.display = "block"
+
+
+}
